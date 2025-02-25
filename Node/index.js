b90e9d12a48eaa98802f7fd7eaa6629e10902b23
janjonaash05 <janjonaash05@gmail.com>
2025-02-25 22:05:56 +0100

    login first steps

--- Node/index.js
+++ Node/index.js
@@ -15,6 +15,41 @@ const dishGW = require("./gateways/dishGateway");
 let lunchArr = []
 
 
+
+app.post("/login", async (req, res) =>
+{
+
+    const { username, password } = req.body;
+
+    let crsfres = await  fetch(" https://strav.nasejidelna.cz/0341/login")
+    let token = crsfres.headers.getSetCookie()[0].split("=")[1];
+
+
+
+    var resauth = await fetch('https://example.com/login', {
+        method: 'POST',
+        headers:{
+            'Host': 'strav.nasejidelna.cz',
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Origin": "https://strav.nasejidelna.cz",
+            "Referer": "https://strav.nasejidelna.cz/0341/login"
+        },
+        body: new URLSearchParams({
+            'j_username': username,
+            'j_password': password,
+            'terminal': 'false',
+            '_csrf':token
+        })
+    });
+
+
+    return res.status(resauth.status).json( await resauth.json());
+
+})
+
+
+
 app.post("/register-daily-menu", async (req,res) =>
 {
     const {dish_id, user_id, portion_size, temperature, visual, taste, smell, extra_payment} = req.body;
